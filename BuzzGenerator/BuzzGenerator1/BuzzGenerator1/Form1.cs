16c0b977bf8757a333db4f456688cc5651b99fe2
Sunil Pitkar <sunil_pitkar@HOTMAIL.COM>
2013-03-19 22:54:37 -0700

    First Check-in in the AED Repository

---
+++ BuzzGenerator/BuzzGenerator1/BuzzGenerator1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuzzGenerator1
+{
+    public partial class BuzzGeneratorForm : Form
+    {
+        private MyTwitter mytwitter = new MyTwitter();
+        Timer DashboardRefreshTimer = new Timer();
+
+        
+        
+        public BuzzGeneratorForm()
+        {
+            InitializeComponent();
+            //TDashboardRefreshTimer = new Timer();
+            //DashboardRefreshTimer.Interval = 5000;
+            //DashboardRefreshTimer.Tick += new EventHandler(DashboardRefreshTimer_Tick);
+            //DashboardRefreshTimer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisableAll();
+            InitializeCounters();
+            Display();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TweetTextBox.Text == "")
+                MessageBox.Show("Please enter some text to tweet");
+            else
+            {
+                MessageBox.Show(TweetTextBox.Text);
+                mytwitter.SendTweet(TweetTextBox.Text);
+                TweetTextBox.Text = "";
+                //Change Lable to "Since Last Tweet"
+                //Display counters to 0
+
+
+            }
+            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Disable all configuration checkboxes and buttons
+            //Clear and Enable tweet textbox and button
+            DisableAll();
+            Enable(TweetTextBox, TweetButton);
+            Display();
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void NoneRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            //Disable all configuration checkboxes and buttons
+            //Determine last retweet and mention and count of pending requests
+            DisableAll();
+            Display();
+
+            
+
+
+        }
+
+        private void DashboardRefreshTimer_Tick(object sender, EventArgs e)
+        {
+            if (sender == DashboardRefreshTimer)
+            {
+               // label1.Text = DateTime.Now.ToLongTimeString();
+            }
+
+        }
+
+        private void AutoImageRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            //Disable all configuration checkboxes and buttons
+            //Clear and Enable Folder textbox and button
+
+        }
+
+        private void AutoTextRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            //Disable all configuration checkboxes and buttons
+            //Clear and Enable File textbox and button
+        }
+
+        private void DisableAll()
+        {
+            TweetTextBox.Text = "";
+            TweetTextBox.ReadOnly = true;
+            TweetButton.Enabled = false;
+            FileTextBox.Text ="";
+            FileTextBox.ReadOnly = true;
+            SelectFileButton.Enabled = false;
+            FolderTextBox.Text = "";
+            FolderTextBox.ReadOnly = true;
+            PicturesFolderButton.Enabled = false;
+                   
+        }
+
+        private void InitializeCounters()
+        {
+            mytwitter.SetInitialCounters();
+
+        }
+
+        private void Enable(TextBox ATextBox,Button AButton)
+        {
+            ATextBox.ReadOnly = false;
+            AButton.Enabled = true;
+
+        }
+
+        private void Display()
+        {
+            RetweetTextBox.Text = mytwitter.Retweets.ToString();
+            MentionsTextBox.Text = mytwitter.Mentions.ToString();
+            FollowRequestTextBox.Text = mytwitter.FollowRequests.ToString();
+
+        }
+    }
+}
---
+++ BuzzGenerator/BuzzGenerator1/BuzzGenerator1/MyTwitter.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+using System.Windows.Forms;
+
+namespace BuzzGenerator1
+{
+    class MyTwitter
+    {
+        private int initialRetweets = 0;
+        private int initialTotalRetweets = 0;
+        private int initialMentions = 0;
+        private int initialFollowRequests = 0;
+        private long lastTweetID = 313821879632793601;
+
+        public int Retweets = 0;
+        public int Mentions = 0;
+        public int FollowRequests = 0;
+
+        private string consumerKey = "bWDpCLO2mIByqMYoaRh2Cg";
+        private string consumerSecret = "X4veUKVWhB09wipWhVpaVhXvLDHqzIjnOLOGXxiY8Y";
+        private string accessToken = "25963011-QuJkeaEcXqB92bhMxoIJHKBLFHP1rPZysu6K68IqZ";
+        private string accessTokenSecret = "pY0TXRW8aIUSDfSUpJAzsQ2s2Oay2A0HBdiaK1Z8";
+
+        public TwitterService service;
+        
+        
+        public MyTwitter()
+        {
+            service = new TwitterService(consumerKey, consumerSecret);
+            service.AuthenticateWith(accessToken, accessTokenSecret);
+            //var service = new TwitterService("tedRo766zL7mr7TKZkOugA", "WREOp5SZ71EtLCt3T4RboUv1IrkUpPkCpcBxkAGk8");
+            //service.AuthenticateWith("21985278-dud1wSertHCQYTUK5ta5AA0ciqWB31ZsT8Dt8DJg", "yPDp2TTOOhQj6XDxX7P5TxmNtHZcQ6sJumth8DVzRk");
+            InitializeMetrics();
+
+        }
+
+        private void InitializeMetrics()
+        {
+            //lastTweetID = GetLastTweetID();
+            GetRetweetsSinceLastTweet();
+            GetMentionsSinceLastTweet();
+
+        }
+
+        private void GetMentionsSinceLastTweet()
+        {
+            ListTweetsMentioningMeOptions myMentionOptions = new ListTweetsMentioningMeOptions();
+            //myRetweetOptions.SinceId = lastTweetID;
+            //myRetweetOptions.Count = 100;
+            myMentionOptions.Count = 200;
+
+
+            var tweets = service.ListTweetsMentioningMe(myMentionOptions);
+
+
+            foreach (var tweet in tweets)
+            {
+                initialMentions = initialMentions + 1;
+
+            }
+            MessageBox.Show(initialMentions.ToString());
+            
+        }
+
+        private void GetRetweetsSinceLastTweet()
+        {
+
+            ListRetweetsOfMyTweetsOptions myRetweetOptions = new ListRetweetsOfMyTweetsOptions();
+            //myRetweetOptions.SinceId = lastTweetID;
+            myRetweetOptions.Count = 100;
+
+            var tweets = service.ListRetweetsOfMyTweets(myRetweetOptions);
+
+
+            foreach (var tweet in tweets)
+            {
+                initialRetweets = initialRetweets + 1;
+                initialTotalRetweets = initialTotalRetweets + tweet.RetweetCount;
+            }
+            //MessageBox.Show( initialRetweets.ToString());
+            //MessageBox.Show( initialTotalRetweets.ToString());
+            
+        }
+
+        private long GetLastTweetID()
+        {
+            ListTweetsOnUserTimelineOptions userTimelineOptions = new ListTweetsOnUserTimelineOptions();
+            userTimelineOptions.Count = 1;
+            var tweets = service.ListTweetsOnUserTimeline(userTimelineOptions); 
+            long lastTweetId=0;
+            foreach (var tweet in tweets)
+            {
+                lastTweetId = tweet.Id;
+            }
+            //MessageBox.Show(lastTweetId.ToString());
+            return lastTweetId;
+        }
+
+        //service.AuthenticateWith(accessToken, accessTokenSecret);
+
+        public void SetInitialCounters()
+        {
+            FollowRequests = 0;
+            Mentions = 0;
+            Retweets = 0;
+        }
+
+        public void SendTweet(String status)
+        { 
+        var tweetoptions = new SendTweetOptions();
+        tweetoptions.Status = status;
+
+        MessageBox.Show(status);
+
+        service.SendTweet(tweetoptions);
+        //service.SendTweet(tweetoptions);
+        
+        }
+
+    }
+}
